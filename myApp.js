25c3b231a5c9ffbfbe0bbed7fb71311fcb64abae
ozgurekinci00 <ozgurekinci95@gmail.com>
2021-05-19 16:23:24 +0300

    fix

--- myApp.js
+++ myApp.js
@@ -12,9 +12,16 @@ app.get("/", function(req, res){
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/json", function(req, res){
-	res.json({
-		"message": "Hello json"
-	});
+	if(process.env.MESSAGE_STYLE === "uppercase"){
+		res.json({
+			"message": "Hello json"
+		});
+	}
+	else{
+		res.json({
+			"message": "HELLO JSON"
+		});
+	}
 });
 
 
