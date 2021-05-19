b279d1eac1f5a12b2ea6740bd487c9c34f5694d2
ozgurekinci00 <ozgurekinci95@gmail.com>
2021-05-19 18:23:21 +0300

    chained middleware added

--- myApp.js
+++ myApp.js
@@ -28,6 +28,15 @@ app.get("/json", function(req, res){
 	}
 });
 
+app.get("/now", function(req, res, next){
+	req.time = new Date().toString();
+	next();
+}, function(req, res){
+	res.json({
+		time: req.time
+	});
+});
+
 
 
 
