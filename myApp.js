e609a978b440eb71b6bb3e9f150472c7f1cc749c
ozgurekinci00 <ozgurekinci95@gmail.com>
2021-05-19 17:55:39 +0300

    fix

--- myApp.js
+++ myApp.js
@@ -24,7 +24,7 @@ app.get("/json", function(req, res){
 	}
 });
 
-app.use((req, res, next) => {
+app.use(function(req, res, next){
 	console.log(req.method + " " + req.path + " - " + req.ip);
 	next();
 });
